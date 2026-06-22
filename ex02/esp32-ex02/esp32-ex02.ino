b1f8f6cb2bf77b9660ce2b596fef8db2e2eefb9e
liangwanqi1005 <2418912939@qq.com>
2026-06-22 17:01:28 +0800

    ex02 v1 millis实现1Hz闪烁

---
+++ ex02/esp32-ex02/esp32-ex02.ino
@@ -0,0 +1,17 @@
+const int ledPin = 2;
+int ledState = LOW;
+unsigned long previousMillis = 0;
+const long interval = 1000;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval/2) {
+    previousMillis = currentMillis;
+    ledState = !ledState;
+    digitalWrite(ledPin, ledState);
+  }
+}
